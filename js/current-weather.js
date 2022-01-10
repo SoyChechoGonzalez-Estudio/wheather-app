99f9b32322412189c49afc2ac42f9c599f2d6187
Sergio González Sánchez <SoySergioGonzalez@Outlook.es>
2022-01-09 23:24:29 -0500

    Se crea funciones para renderizar la fecha y ciudad

---
+++ js/current-weather.js
@@ -0,0 +1,30 @@
+import weather from "../data/current-weather.js";
+import { formatDate } from "./utils/format-data.js";
+
+function setCurrentCity($el, city) {
+  $el.textContent = city;
+}
+
+function setCurrentDate($el) {
+  const date = new Date();
+  const formattedDate = formatDate(date);
+  $el.textContent = formattedDate;
+}
+function configCurrentWeather(weather) {
+  // loader
+  // date
+  const $currentWeatherDate = document.querySelector("#current-weather-date");
+  setCurrentDate($currentWeatherDate);
+  // city
+  const $currentWeatherCity = document.querySelector("#current-weather-city");
+  const city = weather.name;
+  setCurrentCity($currentWeatherCity, city);
+  //temp
+  //background
+}
+
+export default function currentWeather() {
+  // GEO // API - Weather  // Config
+  configCurrentWeather(weather);
+  console.log(weather);
+}
---
+++ js/utils/format-data.js
@@ -0,0 +1,8 @@
+const defaultDateOptions = {
+  day: "numeric",
+  weekday: "long",
+  month: "long",
+};
+export function formatDate(date, options = defaultDateOptions) {
+  return new Intl.DateTimeFormat("es", options).format(date);
+}
